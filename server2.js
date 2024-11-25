f87023bfe7227a6bb2df217d34e371ae49f79275
Ochiponu-Awodi <awodiomale@gmail.com>
2024-11-25 10:29:53 +0100

    Get Req Body For POST

--- server2.js
+++ server2.js
@@ -41,6 +41,25 @@ const getUserByIdHandler = (req, res) => {
     res.end();
 }
 
+// Route Handler for POST api/users
+const createUserHandler = (req, res) => {
+    let body = '';
+    //Listen for the data
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+    })
+}
+
+// Not Found Handler
+
 const notFoundHandler = (req, res) => {
     res.statusCode = 404
     res.write(JSON.stringify({ message: 'Route Not Found' }));
@@ -54,6 +73,8 @@ const server = createServer((req, res) => {
             getUsersHandler(req, res);
         } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
             getUserByIdHandler(req, res);
+        } else if (req.url === '/api/users' && req.method === 'POST') {
+            createUserHandler(req, res);
         } else {
             notFoundHandler(req, res);
         }
